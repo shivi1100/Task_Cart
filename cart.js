592262cbb3fa2b6fd9d17999c24bc100f5185463
Shivangi Shrivastava <147822546+shivi1100@users.noreply.github.com>
2024-02-09 15:28:09 +0530

    Cart_Assignment

---
+++ cart.js
@@ -0,0 +1,29 @@
+function toggleOne(){
+    const card = document.querySelector('table');
+    if(card.style.display === "none") {
+        card.style.display = "block";
+    }
+    else{
+        card.style.display = "none";
+    }
+}
+
+function toggleTwo(){
+    const cardOne = document.querySelector('table.pairTwo')
+    if(cardOne.style.display === "none") {
+        cardOne.style.display = "block";
+    }
+    else{
+        cardOne.style.display = "none"
+    }
+}
+
+function toggleThree(){
+    const cardTwo = document.querySelector('table.pairThree')
+    if(cardTwo.style.display === "none") {
+        cardTwo.style.display = "block";
+    }
+    else{
+        cardTwo.style.display = "none"
+    }
+}
